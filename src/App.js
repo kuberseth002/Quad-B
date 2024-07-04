930ab2a3174ab82ec66d4c2e9940cfb95ea3bbfb
kuberseth002 <103316447+kuberseth002@users.noreply.github.com>
2024-07-04 12:30:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from './component/Nav'
+import Cart from './component/Cart'
+import './App.css'
+
+
+function App() {
+  return (
+    <div>
+        <Nav />
+        <Cart />
+    </div>
+  )
+}
+
+export default App
